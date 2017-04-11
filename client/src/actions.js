0914ea60e6f5cdf428c8825980f6ef9d4787430a
miclovich <vicmiclovich@gmail.com>
2017-04-11 18:04:03 +0300

    dispatch success

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,14 +1,16 @@
 // const uid = () => Math.random().toString(34).slice(2);
 import axios from 'axios';
 
-export const fetchDescriptionSuccess = (podcast, description) => ({type: 'FETCH_DESCRIPTION_SUCCESS', podcast, description});
+export const fetchDescriptionSuccess = (podcast, data) => ({type: 'FETCH_DESCRIPTION_SUCCESS', podcast, data});
 
 export const addPodcast = podcast => dispatch => {
   const url = `/search?q=${encodeURI(podcast)}`  // backend request.query['q']
   return axios.get(url)
     .then(function (data) {
-      console.log('my data', data)})
-      .catch(function (error) {
+      console.log('my data', data)
+      dispatch(fetchDescriptionSuccess(podcast, data))
+    })
+    .catch(function (error) {
         console.log(error)
       })
   // return fetch(url).then(response => {
