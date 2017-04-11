ab6de419b13be6becffebcab5a26ea3bfa6180df
miclovich <vicmiclovich@gmail.com>
2017-04-11 18:06:48 +0300

    cleanup

--- client/src/actions.js
+++ client/src/actions.js
@@ -12,33 +12,9 @@ export const addPodcast = podcast => dispatch => {
     })
     .catch(function (error) {
         console.log(error)
-      })
-  // return fetch(url).then(response => {
-  //         if (!response.ok) {
-  //             const error = new Error(response.statusText)
-  //             error.response = response
-  //             throw error;
-  //         }
-  //         console.log(response, 'what!!');
-  //
-  //     }).then(response => {
-  //       console.log('something should happen', response.body);
-  //       // return response.json()
-  //       return {};
-  //     }).then(data => {
-  //       console.log('steve data', data)
-  //       dispatch(fetchDescriptionSuccess(podcast, data))
-  //       // return {
-  //       //   type: 'ADD_PODCAST',
-  //       //   payload: {
-  //       //     name: podcast
-  //       //   }
-  //       // };
-  //
-  //     }).catch(error => {
-  //       console.log('caught', error);
-  //       // dispatch(fetchDescriptionError(podcast, error))
-  //     });
+        // TODO -> steve fix this
+        // dispatch(fetchDescriptionError(podcast, error))
+      })  
 }
 
 export function toggleTodo(id) {
@@ -47,6 +23,3 @@ export function toggleTodo(id) {
     payload: id
   };
 }
-
-
-// export const FETCH_DESCRIPTION_SUCCESS = 'FETCH_DESCRIPTION_SUCCESS';
