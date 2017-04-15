ed9025a4938fbc121969b6e40e77af9f8cfd092b
steviebuckets <steviebuckets33@gmail.com>
2017-04-15 14:15:54 -0700

    updated landing page and routes and buttons

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import {Link} from 'react-router';
+import { Card, CardText } from 'material-ui/Card';
+
+// export const App = (props) => {
+//     return (
+//         <div className="navbar">
+//             <div className="logo">
+//                 <h3>Pod-Feed</h3>
+//
+//                 <Link className='btn btn-success' to='/signin'>
+//                     Sign In
+//                 </Link>
+//
+//                 <Link className='btn btn-primary' to='/register'>
+//                     Register
+//                 </Link>
+//                 <div>
+//                     {props.children}
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
 export const App = (props) => {
-    return (
-        <div className="navbar">
-            <div className="logo">
-                <h3>Pod-Feed</h3>
+  return(
+      <Card className='container-links'>
+      <CardText><Link to={'/signin'}>Sign in</Link></CardText>
+      <CardText><Link to={'/register'}>Register</Link></CardText>
+      {props.children}
+      </Card>
 
-                <Link className='btn btn-success' to='/signin'>
-                    Sign In
-                </Link>
-
-                <Link className='btn btn-primary' to='/register'>
-                    Register
-                </Link>
-                <div>
-                    {props.children}
-                </div>
-            </div>
-        </div>
-    )
+  )
 }
+
+// <Link to={'/signin'}</Link>
+//
+// // Link={'/signin'}
+//
+// // <Link to={'/signin'}</Link>
+// //
+// //
+// // // <div className="button-line">
+// // //         <RaisedButton type="submit" label="Log in" primary />
+// // //       </div>
+// // //
+// // //       <CardText>Don't have an account? <Link to={'/signup'}>Create one</Link>.</CardText>
