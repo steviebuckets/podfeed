72c397513d84c9cd72f320631b818723fbbf0549
steviebuckets <steviebuckets33@gmail.com>
2017-05-13 11:10:51 -0700

    added masonry to podcast-list compo and revamped grid layout

--- client/src/components/podcast-list.js
+++ client/src/components/podcast-list.js
@@ -1,38 +1,65 @@
 import React from 'react';
 import Podcast from './podcast';
-import {GridList, GridTile} from 'material-ui/GridList';
+const Masonry = require('react-masonry-component');
+// import {GridList, GridTile} from 'material-ui/GridList';
 // import IconButton from 'material-ui/IconButton';
-import Subheader from 'material-ui/Subheader';
+// import Subheader from 'material-ui/Subheader';
 // import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
+// const styles = {
+//   root: {
+//     display: 'flex',
+//     flexWrap: 'wrap',
+//     justifyContent: 'space-around',
+//   },
+//   gridList: {
+//     width: 800,
+//     height: 800,
+//     overflowY: 'auto',
+//   },
+// };
 
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-  },
-  gridList: {
-    width: 800,
-    height: 800,
-    overflowY: 'auto',
-  },
+const masonryOptions = {
+
+
+
+};
+
+const style = {
+
 };
 
 export class PodcastList extends React.Component {
-  constructor(props) {
-    super(props)
-      this.audio = "";
-      this.addPodcast = "";
-      this.onSubmit = this.onSubmit.bind(this);
+    constructor(props) {
+        super(props)
+        this.audio = "";
+        this.addPodcast = "";
+        this.onSubmit = this.onSubmit.bind(this);
 
-  }
+        //trying to append a row and column
+        // this.cast = '<Podcast podcast={podcast}/>';
+        // this.items = '<div className="mui-col-md-4">';
+        // this.row = '<div className="mui-row">';
+
+    }
+
+    // this.items.append(cast);
+    //
+    // ('mui-col-md-4').append(items);
+
     // console.log('props', props);
 
     // this.props = {audio, addPodcast}// {audio: 'blah', addPodcast: 'meh'}
     componentDidMount() {
-      this.props.addPodcast('Interview')
+        this.props.addPodcast('Interview')
     }
+
+    // podcastGrid(props) {
+    //   // const cast= '<Podcast podcast={podcast}/>';
+    //   // const items = '<div class="col-md-4"></div>';
+    //   this.props.items.append(this.cast);
+    //   this.props.row.append(this.items);
+    // }
 
     onSubmit(event) {
         const input = event.target;
@@ -47,44 +74,72 @@ export class PodcastList extends React.Component {
         }
     };
 
+    // <input type='text' className='podcast__name' placeholder='Search Podcast' onKeyDown={this.onSubmit}/>
+    // // returns a podcast
+    // <Podcast podcast={podcast}/>
 
-render() {
-  return (
-    <div style={styles.root}>
-      <GridList
-        cellHeight={400}
-        style={styles.gridList}
-      >
-      <Subheader>Featured</Subheader>
-      <input type='text' className='podcast__name' placeholder='Search Podcast' onKeyDown={this.onSubmit}/>
-      {this.props.audio.podcastReducer.map(podcast => (
-        <GridTile>
-        <ul key={podcast.url} className='podcast__item'>
-            <Podcast podcast={podcast}/>
-        </ul>
-        </GridTile>
-      ))}
-    </GridList>
-  </div>
+    render() {
+        return (
+            <Masonry className={'my-gallery-class'} style={style} onClick={this.handleClick} options={masonryOptions} elementType={'ul'}>
 
-)
+              {this.props.audio.podcastReducer.map(podcast => (
+                <a key={podcast.url} className='podcast__item'>
+                   <Podcast podcast={podcast}/>
+                   </a>
+                   ))}
+            </Masonry>
+
+
+        //3rd component with podcasts in a bootstrap grid
+        // <div className="mui-container-fluid">
+        // {this.props.audio.podcastReducer.map(podcast => (
+        //     <div className="mui-row">
+        //       <ul key={podcast.url} className='podcast__item'>
+        //         <div className="mui-col-md-4">
+        //             <Podcast podcast={podcast}/>
+        //         </div>
+        //          </ul>
+        //
+        //     </div>
+        //   ))}
+        // </div>
+
+        // second component with podcasts in a GridList
+        //   <div style={styles.root}>
+        //     <GridList
+        //       cellHeight={400}
+        //       style={styles.gridList}
+        //     >
+        //     <Subheader><p>FEATURED</p></Subheader>
+        //     <Subheader><p>YOUR PODCASTS</p></Subheader>
+        //
+        //     {this.props.audio.podcastReducer.map(podcast => (
+        //       <GridTile>
+        //       <ul key={podcast.url} className='podcast__item'>
+        //           <Podcast podcast={podcast}/>
+        //       </ul>
+        //       </GridTile>
+        //     ))}
+        //   </GridList>
+        // </div>
+
+        )
+    }
+
+    //original component with podcasts
+    //     render(){
+    //     return (
+    //         <div className='podcast-list'>
+    //         <h2>Featured</h2>
+    //             <input type='text' className='podcast__name' placeholder='Search Podcast' onKeyDown={this.onSubmit}/>
+    //             <ul className='podcast__list'>
+    //                 {this.props.audio.podcastReducer.map(podcast => (
+    //                     <li key={podcast.url} className='podcast__item'>
+    //                         <Podcast podcast={podcast}/>
+    //                     </li>
+    //                 ))}
+    //             </ul>
+    //         </div>
+    //     );
+    //   }
 }
-
-
-
-//     render(){
-//     return (
-//         <div className='podcast-list'>
-//         <h2>Featured</h2>
-//             <input type='text' className='podcast__name' placeholder='Search Podcast' onKeyDown={this.onSubmit}/>
-//             <ul className='podcast__list'>
-//                 {this.props.audio.podcastReducer.map(podcast => (
-//                     <li key={podcast.url} className='podcast__item'>
-//                         <Podcast podcast={podcast}/>
-//                     </li>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-//   }
-  }
--- client/src/components/podcast.js
+++ client/src/components/podcast.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
+
 export default function Podcast(props) {
-    console.log('my props', props)
     const {podcast} = props;
     return (
         <div className="podcast">
-            {podcast.name}<br/>
+            
             <a href={podcast.url} className="link">
                 <br/>
                 <img src={podcast.pictures.large} alt="cover"/>
