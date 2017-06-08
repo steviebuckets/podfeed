b9f4371c9ee752290edd8f392c2fe5e94aba682f
steviebuckets <steviebuckets33@gmail.com>
2017-06-08 13:46:58 -0700

    removed old comments

--- client/src/reducers/register-reducer.js
+++ client/src/reducers/register-reducer.js
@@ -14,37 +14,3 @@ export default function registerReducer(state = {}, action) {
   }
 
   }
-
-  // export default function userReducer(state = {}, action) {
-  //   switch (action.type) {
-  //     case LOGIN_SUCCESS:
-  //       console.log('action', action);
-  //       let token = action.payload.data.token;
-  //       localStorage.setItem('token', token);
-  //       return Object.assign({}, state, {
-  //         token: token
-  //       })
-  //       default:
-  //         return state;
-  //   }
-  //
-  //   }
-
-
-  // Register User Action
-  // export const verifyUser = user => dispatch => {
-  //   // console.log('my user', user)
-  //   axios.post('/register', {
-  //     email: user.email,
-  //     password: user.password
-  //   }).then(function(response) {
-  //
-  //     let myToken = response.data.token;
-  //     localStorage.setItem('token', myToken);
-  //     location.replace('/');
-  //
-  //   }).catch(function(error) {
-  //     console.log('User already exists', error);
-  //   })
-  // }
-  //
