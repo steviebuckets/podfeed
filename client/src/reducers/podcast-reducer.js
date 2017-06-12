f5b6f1961624085395bad3482f6aeabeb90a66ec
steviebuckets <steviebuckets33@gmail.com>
2017-06-12 09:05:05 -0700

    removed podcasts array in fecth description success to remove subscriptions from showing up as duplicates in podlist component

--- client/src/reducers/podcast-reducer.js
+++ client/src/reducers/podcast-reducer.js
@@ -5,23 +5,27 @@ import {FETCH_DESCRIPTION_SUCCESS, FETCH_USER_SUBSCRIPTION_SUCCESS, FETCH_STILL_
 export default function podcastReducer(podcasts = init, action) {
   // console.log('what does action', podcasts);
   console.log(action, 'action');
-
   switch (action.type) {
       // console.log(action, 'url');
+    //Performs a Podcast Search
     case 'ADD_PODCAST':
       return [
         action.payload.data.data, ...podcasts
       ]
+    //Returns Podcast Data from Search
     case FETCH_DESCRIPTION_SUCCESS:
       return [
-        ...podcasts,
+
         ...action.payload.data.data
-      ]; // double spread operators do the trickz
+      ];
+    //This Returns User Podcast Subscriptions
     case FETCH_USER_SUBSCRIPTION_SUCCESS:
       console.log(action, "my subscriptions actions")
       return [...action.payload.data];
+    //New Podcast Subscriptions
     case SUBSCRIBED_SUCCESS:
       return action.payload.data
+    //Loading Page
     case FETCH_STILL_LOADING:
       return podcasts // this being empty we can get that lovely "spinner action"
     default:
@@ -29,26 +33,3 @@ export default function podcastReducer(podcasts = init, action) {
   }
 
 }
-
-// let init = [];
-// import {FETCH_DESCRIPTION_SUCCESS, LOGIN_SUCCESS} from '../actions';
-// // create object for user
-// // another for podcasts
-// // create another red
-// export default function podcastReducer(podcasts = init, action) {
-//   // console.log('what does action', action.type);
-//   switch (action.type) {
-//     case 'ADD_PODCAST':
-//       console.log(action, 'add');
-//       return [action.payload, ...podcasts]
-//     case FETCH_DESCRIPTION_SUCCESS:
-//       console.log('what?', action);
-//       // podcasts = action.description.data.data;
-//       // return  [action.payload.data.data, ...podcasts];
-//       return action.payload.data.data;
-//     case 'FETCH_USER_SUBSCRIPTION_SUCCESS':
-//      return [action.payload]
-//     default:
-//       return podcasts;
-//   }
-// }
