5ca84365e51d23533eacb0dd158e2242ffaf6868
miclovich <vicmiclovich@gmail.com>
2017-06-16 15:31:46 +0300

    making code shorter

--- client/src/containers/podcast-list-container.js
+++ client/src/containers/podcast-list-container.js
@@ -1,26 +1,13 @@
-import {connect} from 'react-redux';
-import {addPodcast, newSubscription, userSubscriptions} from '../actions';
+import { connect } from 'react-redux';
+import { addPodcast, newSubscription, userSubscriptions } from '../actions';
 import * as components from '../components/podcast-list';
-import { bindActionCreators } from 'redux';
 
 const mapStateToProps = (state) => {
-  // console.log(state, "my state")
-  return {audio: state}
+  return { audio: state }
 }
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({addPodcast, newSubscription, userSubscriptions}, dispatch)
-  // return {
-  //   addPodcast: text => dispatch(addPodcast(text)),
-  //   newSubscription: key => dispatch(newSubscription(key)),
-  //   userSubscriptions: user => dispatch(userSubscriptions(user))
-  // }
-}
-
-// export default connect(mapStateToProps, {addPodcast, newSubscription, userSubscriptions})(components.PodcastList)
 
 const PodcastList = connect(
   mapStateToProps,
-  mapDispatchToProps
+  { addPodcast, newSubscription, userSubscriptions }
 )(components.PodcastList)
 export default PodcastList
